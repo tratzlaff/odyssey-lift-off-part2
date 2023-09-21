5715696356c761a9f1a3cc73ee59e9eee8cf098b
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-09-21 17:02:38 -0500

    Our server is now fully configured to work with live data. To learn more about options that Apollo Server can receive, see https://www.apollographql.com/docs/apollo-server/api/apollo-server/#options

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,38 @@ const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const typeDefs = require('./schema');
 const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api"); // note the PascalCase convention, as we're dealing with the class here
 
 async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    // This object (second param of startStandaloneServer) is for configuring your server's options.
+
+    // Define a context function that returns an object that all our resolvers will share
+    context: async () => {
+      // This object becomes our resolver's contextValue, the third positional argument.
+
+      // To take advantage of the RESTDataSource's caching capabilities,
+      // we need to pass in the server's cache to our TrackAPI.
+      const { cache } = server;
+
+      return {
+        // We want to access the dataSources.trackAPI (and its methods) from the contextValue
+        // parameter of our resolvers. So let's return an object that allows us to do just that!
+        // Note: Our resolver functions expect to find dataSources.trackAPI on their contextValue,
+        // which is why we've defined a property called dataSources here in our server.
+        // This particular name isn't a requirement - we chose dataSources as a matter of convention.
+        // You can give this property whatever name you'd like, but be sure that you update your resolver
+        // functions to access the same property.
+        dataSources: {
+          trackAPI: new TrackAPI({ cache }),
+        },
+      };
+    },
+  });
   console.log(`
       🚀  Server is running
       📭  Query at ${url}
