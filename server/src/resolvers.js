4afab78ce46ea58f295eb5c4882e164e7b331e64
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-09-21 17:11:34 -0500

    Bug fix.

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -9,18 +9,18 @@ const resolvers = {
             // (lowercase here as it's the instance of our TrackAPI class extending RESTDataSource)
             return dataSources.trackAPI.getTracksForHome();
 
+        }
+    },
+    Track: {
+        author: ({authorId}, _, {dataSources}) => {
+            // We get the needed authorId from the parent argument passed to the resolver.
+            // The parent argument contains data returned by our tracksForHome resolver, and
+            // because tracksForHome returns a list, Apollo Server iterates through the list
+            // and calls the author resolver for each item, passing the item as the parent.
+            return dataSources.trackAPI.getAuthor(authorId);
         },
+    },
 
-        Track: {
-            author: ({authorId}, _, {dataSources}) => {
-                // We get the needed authorId from the parent argument passed to the resolver.
-                // The parent argument contains data returned by our tracksForHome resolver, and
-                // because tracksForHome returns a list, Apollo Server iterates through the list
-                // and calls the author resolver for each item, passing the item as the parent.
-                return dataSources.trackAPI.getAuthor(authorId);
-            },
-        },
-    }
 };
 
 module.exports = resolvers;
