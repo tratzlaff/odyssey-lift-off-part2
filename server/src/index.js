89a4c8b4ff4339b7d24f0cd3d35594b5f217ead6
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-09-21 16:53:52 -0500

    Take care of resolvers.

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,13 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
 const typeDefs = require('./schema');
+const resolvers = require("./resolvers");
 
 async function startApolloServer() {
-  const server = new ApolloServer({});
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
   const { url } = await startStandaloneServer(server);
   console.log(`
       🚀  Server is running
