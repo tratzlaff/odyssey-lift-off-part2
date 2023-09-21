60552d9c82d5dd1cc84b82b1830ab791f4f077ab
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-09-21 16:46:46 -0500

    Implementing query resolvers. Note: Now, you might be wondering where the dataSources object that's passed to our resolver comes from. When did we add it to the contextValue parameter? Right now, it isn't added. If we ran our server, it would have no clue about our TrackAPI. It also doesn't know about our resolvers yet!

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,8 +3,22 @@ const resolvers = {
     Query: {
         // This resolver function returns an array of Tracks.
         // Resolver functions have four optional parameters: parent, args, contextValue, and info.
-        tracksForHome: (parent, args, contextValue, info) => {
+        tracksForHome: (_, __, {dataSources}) => {
 
+            // From our dataSources object, we'll gain access to our trackAPI
+            // (lowercase here as it's the instance of our TrackAPI class extending RESTDataSource)
+            return dataSources.trackAPI.getTracksForHome();
+
+        },
+
+        Track: {
+            author: ({authorId}, _, {dataSources}) => {
+                // We get the needed authorId from the parent argument passed to the resolver.
+                // The parent argument contains data returned by our tracksForHome resolver, and
+                // because tracksForHome returns a list, Apollo Server iterates through the list
+                // and calls the author resolver for each item, passing the item as the parent.
+                return dataSources.trackAPI.getAuthor(authorId);
+            },
         },
     }
 };
