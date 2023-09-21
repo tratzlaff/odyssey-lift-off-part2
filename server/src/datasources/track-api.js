f3391d75841e8e8d2fb2d6d4df3c8ab199fb5ae3
Tanner Ratzlaff <tratzlaff@gmail.com>
2023-09-21 15:22:51 -0500

    Implementing our RESTDataSource.

---
+++ server/src/datasources/track-api.js
@@ -0,0 +1,20 @@
+const { RESTDataSource } = require("@apollo/datasource-rest"); // Gives us a RESTDataSource class that we can extend.
+
+class TrackAPI extends RESTDataSource {
+
+    // This property is used as the prefix to all the calls.
+    baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
+
+    getTracksForHome() {
+        // See https://www.apollographql.com/docs/apollo-server/data/fetching-rest/#http-methods
+        return this.get('tracks');
+    }
+
+    getAuthor(authorId) {
+        // See https://www.apollographql.com/docs/apollo-server/data/fetching-rest/#http-methods
+        // Note the use of backticks (`), because we're using string interpolation to add the authorId at the end.
+        return this.get(`author/${authorId}`);
+    }
+
+
+}
